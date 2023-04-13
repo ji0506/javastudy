a8c3f896c2faaf8534c27bf0988dc97646cd8e19
jiwan <audwldhks0@gmail.com>
2023-04-13 11:40:30 +0900

    덧셈변수

---
+++ ch00/sec01/Test01.java
@@ -0,0 +1,11 @@
+package ch00.sec01;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		int b = 4;
+		System.out.println("a+b=" + (a+b));		
+	}
+}
